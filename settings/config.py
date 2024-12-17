141f8d6b5600b1a32568cb8d509883b47f07f17a
nehabiswal1 <nb295@njit.edu>
2024-12-16 23:50:32 -0500

    removed those old files

--- settings/config.py
+++ settings/config.py
@@ -6,6 +6,11 @@ class Settings(BaseSettings):
     mailtrap_port: int
     mailtrap_username: str
     mailtrap_password: str
+    minio_endpoint: str
+    minio_access_key: str
+    minio_secret_key: str
+    minio_bucket_name: str
+    minio_public_endpoint: str
 
     class Config:
         env_file = ".env"  # Specifies the location of the .env file
